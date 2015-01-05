9500c49f1cf6a4a150485f49c1db1bb381deaa31
Corey Goldberg <cgoldberg@gmail.com>
2015-01-05 12:18:29 -0500

    initial import

---
+++ ttfb-timing/ttfb_collector.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""measure http time to first byte (ttfb) latency for cdn hosted videos"""
+
+import json
+import time
+from timeit import default_timer
+import urllib2
+
+
+TIME_INTERVAL = 10  # secs
+RESULTS = 'results.log'
+ERROR = 'errors.log'
+
+
+def log_error(human_time, epoch, url, error_msg):
+    msg = '{}, {}, {}, {}'.format(human_time, epoch, url, error_msg)
+    with open(ERROR, 'a') as f:
+        f.write('{}\n'.format(msg))
+    print(msg)
+
+
+def log_success(human_time, epoch, url, ttfb):
+    msg = '{}, {}, {}, {}'.format(human_time, epoch, url, ttfb)
+    with open(RESULTS, 'a') as f:
+        f.write('{}\n'.format(msg))
+    print(msg)
+
+
+def get_xuetang_api_url(url):
+    try:
+        resp = urllib2.urlopen(url)
+        data = resp.read()
+        resp.close()
+    except urllib2.HTTPError as e:
+        log_error(human_time, epoch, url, e.code)
+    except urllib2.URLError as e:
+        log_error(human_time, epoch, url, e.reason)
+    json_data = json.loads(data)
+    return json_data[u'sources'][0]
+
+
+def get_ttfb(url):
+    human_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+    epoch = time.time()
+    start_time = default_timer()
+    try:
+        resp = urllib2.urlopen(url)
+        resp.read(1)
+        resp.close()
+    except urllib2.HTTPError as e:
+        log_error(human_time, epoch, url, e.code)
+    except urllib2.URLError as e:
+        log_error(human_time, epoch, url, e.reason)
+    else:
+        end_time = default_timer()
+        ttfb = (end_time - start_time) * 1000
+        log_success(human_time, epoch, url, ttfb)
+
+
+if __name__ == '__main__':
+    while True:
+        urls = []
+        urls.append('http://video.study.163.com/edu-video/nos/mp4/2014/06/23/460097_hd.mp4')
+        urls.append('http://d2f1egay8yehza.cloudfront.net/TSGCMATH/TSGCMATHT314-V000400_DTH.mp4')
+        lookup_url = 'http://api.xuetangx.com/edx/video?s3_url=https://s3.amazonaws.com/edx-course-videos/mit-600x/M-600X-FA12-L3-Intro_100.mp4'
+        urls.append(get_xuetang_api_url(lookup_url))
+        for url in urls:
+            get_ttfb(url)
+        time.sleep(TIME_INTERVAL)
---
+++ ttfb-timing/ttfb_plot_stats.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import csv
+import urlparse
+
+import numpy as np
+from bokeh.plotting import *
+
+
+input = 'results-china3.log'
+
+class Stats:
+
+    def __init__(self, csv_file):
+        self.loaded_stats = []
+        with open(input) as f:
+            for i, row in enumerate(csv.reader(f)):
+                if i == 0:
+                    epoch_time = float(row[1])
+                elapsed_time = float(row[1]) - float(epoch_time)
+                url = row[2]
+                timer = float(row[3])
+                print timer
+                self._load_stat(url, elapsed_time, timer)
+        self.unique_urls = list(set(urlparse.urlparse(stat[0]).path.strip() for stat in self.loaded_stats))
+        self.unique_url_netlocs = [urlparse.urlparse(url).netloc for url in self.unique_urls]
+
+    def _load_stat(self, url, elapsed_time, timer):
+        self.loaded_stats.append((url, elapsed_time, timer))
+
+    def _get_axis(self, netloc):
+        x = [line[1] for line in self.loaded_stats if netloc in line[0]]
+        y = [line[2] for line in self.loaded_stats if netloc in line[0]]
+        return x, y
+
+    def print_stats_report(self):
+        print 'ttfb (times in milliseconds):\n'
+        for netloc in self.unique_url_netlocs:
+            _, y = self._get_axis(netloc)
+            print '{}:'.format(netloc)
+            print '    99th pct: {:.0f}'.format(np.percentile(y, 99))
+            print '    95th pct: {:.0f}'.format(np.percentile(y, 95))
+            print '    90th pct: {:.0f}'.format(np.percentile(y, 90))
+            print '    80th pct: {:.0f}'.format(np.percentile(y, 80))
+            print '    70th pct: {:.0f}'.format(np.percentile(y, 70))
+            print '    60th pct: {:.0f}'.format(np.percentile(y, 60))
+            print '    median: {:.0f}'.format(np.median(y))
+            print '    mean: {:.0f}'.format(np.mean(y))
+            print '    stdev: {:.0f}'.format(np.std(y))
+            print ''
+
+    def plot_to_html(self):
+        output_file('scatter_plots.html', title='scatter plots')
+        tools = 'pan,wheel_zoom,box_zoom,reset,save,box_select'
+        width = 800
+        height = 400
+        vbox = VBox()
+        for netloc in self.unique_url_netlocs:
+            x, y = self._get_axis(netloc)
+            p = figure(tools=tools,
+                       title=netloc,
+                       plot_width=width,
+                       plot_height=height,
+                       y_range=[0.0, 4000.0],
+                       x_axis_label = 'elapsed time in test (seconds)',
+                       y_axis_label = 'ttfb latency (milliseconds)',
+                      )
+            p.scatter(x, y, color='blue')
+            vbox.children.append(p)
+        show(vbox)
+
+
+if __name__ == '__main__':
+    csv_file = 'results-china2.csv'
+    stats = Stats(csv_file)
+    stats.print_stats_report()
+    stats.plot_to_html()
+
